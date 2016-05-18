5eb01b4c8892b8a386838d32d0a60d092b08f242
Davide Rizzi <d.rizzi@novalabs.io>
2016-05-18 11:31:11 +0200

    New code appearance + Copyright

--- include/PIDNode.hpp
+++ include/PIDNode.hpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
 #pragma once
 
 #include <Core/MW/Publisher.hpp>
@@ -12,50 +18,50 @@
 #include <pid/PID.hpp>
 
 namespace pid {
-	class PIDNode:
-		public Core::MW::CoreNode,
-		                 PID
-	{
+   class PIDNode:
+      public Core::MW::CoreNode,
+                       PID
+   {
 public:
-		PIDNode(
-				const char*                    name,
-				Core::MW::Thread::PriorityEnum priority = Core::MW::Thread::PriorityEnum::NORMAL
-		);
-		virtual
-		~PIDNode();
+      PIDNode(
+         const char*                    name,
+         Core::MW::Thread::PriorityEnum priority = Core::MW::Thread::PriorityEnum::NORMAL
+      );
+      virtual
+      ~PIDNode();
 
 public:
-		PIDNodeConfiguration configuration;
+      PIDNodeConfiguration configuration;
 
 private:
-		bool
-		onConfigure();
+      bool
+      onConfigure();
 
-		bool
-		onPrepareMW();
+      bool
+      onPrepareMW();
 
-		bool
-		onLoop();
+      bool
+      onLoop();
 
-		static bool
-		setpoint_callback(
-				const common_msgs::Float32& msg,
-				Core::MW::Node*             node
-		);
+      static bool
+      setpoint_callback(
+         const common_msgs::Float32& msg,
+         Core::MW::Node*             node
+      );
 
-		static bool
-		measure_callback(
-				const common_msgs::Float32& msg,
-				Core::MW::Node*             node
-		);
+      static bool
+      measure_callback(
+         const common_msgs::Float32& msg,
+         Core::MW::Node*             node
+      );
 
 
 private:
-		Core::MW::Subscriber<common_msgs::Float32, 5> _setpoint_subscriber;
-		Core::MW::Subscriber<common_msgs::Float32, 5> _measure_subscriber;
-		Core::MW::Publisher<common_msgs::Float32>     _output_publisher;
-		float _setpoint;
-		Core::MW::Time _setpoint_timestamp;
-		float          _measure;
-	};
+      Core::MW::Subscriber<common_msgs::Float32, 5> _setpoint_subscriber;
+      Core::MW::Subscriber<common_msgs::Float32, 5> _measure_subscriber;
+      Core::MW::Publisher<common_msgs::Float32>     _output_publisher;
+      float _setpoint;
+      Core::MW::Time _setpoint_timestamp;
+      float          _measure;
+   };
 }
--- src/PIDNode.cpp
+++ src/PIDNode.cpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
 #include <Module.hpp>
 
 #include <pid/PIDNode.hpp>
@@ -5,94 +11,94 @@
 #include <Core/Utils/Math/Conversions.hpp>
 
 namespace pid {
-	PIDNode::PIDNode(
-			const char*                    name,
-			Core::MW::Thread::PriorityEnum priority
-	) :
-		CoreNode::CoreNode(name, priority),
-		_setpoint(0),
-		_measure(0)
-	{
-		_workingAreaSize = 512;
-	}
+   PIDNode::PIDNode(
+      const char*                    name,
+      Core::MW::Thread::PriorityEnum priority
+   ) :
+      CoreNode::CoreNode(name, priority),
+      _setpoint(0),
+      _measure(0)
+   {
+      _workingAreaSize = 512;
+   }
 
-	PIDNode::~PIDNode()
-	{
-		teardown();
-	}
+   PIDNode::~PIDNode()
+   {
+      teardown();
+   }
 
-	bool
-	PIDNode::onConfigure()
-	{
-		config(configuration.kp, configuration.ti, configuration.td, configuration.ts, configuration.min, configuration.max);
+   bool
+   PIDNode::onConfigure()
+   {
+      config(configuration.kp, configuration.ti, configuration.td, configuration.ts, configuration.min, configuration.max);
 
-		return true;
-	}
+      return true;
+   }
 
-	bool
-	PIDNode::onPrepareMW()
-	{
-		_setpoint_subscriber.set_callback(PIDNode::setpoint_callback);
-		_measure_subscriber.set_callback(PIDNode::measure_callback);
+   bool
+   PIDNode::onPrepareMW()
+   {
+      _setpoint_subscriber.set_callback(PIDNode::setpoint_callback);
+      _measure_subscriber.set_callback(PIDNode::measure_callback);
 
-		this->subscribe(_setpoint_subscriber, configuration.setpoint_topic);
-		this->subscribe(_measure_subscriber, configuration.measure_topic);
+      this->subscribe(_setpoint_subscriber, configuration.setpoint_topic);
+      this->subscribe(_measure_subscriber, configuration.measure_topic);
 
-		this->advertise(_output_publisher, configuration.output_topic);
+      this->advertise(_output_publisher, configuration.output_topic);
 
-		return true;
-	}
+      return true;
+   }
 
-	bool
-	PIDNode::onLoop()
-	{
-		if (!this->spin(Configuration::SUBSCRIBER_SPIN_TIME)) {
-			Module::led.toggle();
-		}
+   bool
+   PIDNode::onLoop()
+   {
+      if (!this->spin(Configuration::SUBSCRIBER_SPIN_TIME)) {
+         Module::led.toggle();
+      }
 
-		if ((this->_setpoint_timestamp + configuration.timeout) > Core::MW::Time::now()) {
-			common_msgs::Float32* msgp;
+      if ((this->_setpoint_timestamp + configuration.timeout) > Core::MW::Time::now()) {
+         common_msgs::Float32* msgp;
 
-			if (_output_publisher.alloc(msgp)) {
-				msgp->value = configuration.idle;
-				_output_publisher.publish(*msgp);
-			}
-		}
+         if (_output_publisher.alloc(msgp)) {
+            msgp->value = configuration.idle;
+            _output_publisher.publish(*msgp);
+         }
+      }
 
-		return true;
-	} // PID::onLoop
+      return true;
+   } // PID::onLoop
 
-	bool
-	PIDNode::setpoint_callback(
-			const common_msgs::Float32& msg,
-			Core::MW::Node*             node
-	)
-	{
-		PIDNode* _this = static_cast<PIDNode*>(node);
+   bool
+   PIDNode::setpoint_callback(
+      const common_msgs::Float32& msg,
+      Core::MW::Node*             node
+   )
+   {
+      PIDNode* _this = static_cast<PIDNode*>(node);
 
-		_this->_setpoint = msg.value;
-		_this->_setpoint_timestamp = Core::MW::Time::now();
-		_this->set(_this->_setpoint);
+      _this->_setpoint = msg.value;
+      _this->_setpoint_timestamp = Core::MW::Time::now();
+      _this->set(_this->_setpoint);
 
-		return true;
-	}
+      return true;
+   }
 
-	bool
-	PIDNode::measure_callback(
-			const common_msgs::Float32& msg,
-			Node*                       node
-	)
-	{
-		PIDNode* _this = static_cast<PIDNode*>(node);
-		common_msgs::Float32* msgp;
+   bool
+   PIDNode::measure_callback(
+      const common_msgs::Float32& msg,
+      Node*                       node
+   )
+   {
+      PIDNode* _this = static_cast<PIDNode*>(node);
+      common_msgs::Float32* msgp;
 
-		_this->_measure = msg.value;
+      _this->_measure = msg.value;
 
-		if (_this->_output_publisher.alloc(msgp)) {
-			msgp->value = _this->update(_this->_measure);
-			_this->_output_publisher.publish(*msgp);
-		}
+      if (_this->_output_publisher.alloc(msgp)) {
+         msgp->value = _this->update(_this->_measure);
+         _this->_output_publisher.publish(*msgp);
+      }
 
-		return true;
-	}
+      return true;
+   }
 }
